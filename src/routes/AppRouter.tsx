6e23cec2a92ad6bfd4b45864635c1d97709a1adb
Mati-ERojas <matiasemanuelrojas0112@gmail.com>
2026-02-20 19:04:51 -0300

    .

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Home } from '../components/screens/Home/Home';
 
 export const AppRouter = () => {
-    const location = useLocation();
 
   return (
     <>
