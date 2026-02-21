6deb1743fccb82a718bbd3804ec8bb23a84a3632
Mati-ERojas <matiasemanuelrojas0112@gmail.com>
2026-02-20 21:44:12 -0300

    Cambio en rutas

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,13 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
-import { Home } from '../components/screens/Home/Home';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Home } from "../components/screens/Home/Home";
 
 export const AppRouter = () => {
-
-  return (
-    <>
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-    </>
-  )
-}
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" replace />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </>
+    );
+};
