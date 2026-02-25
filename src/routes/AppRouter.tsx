cff9e80654aad5b47c864cd45c64ff4de93b03a1
Mati-ERojas <matiasemanuelrojas0112@gmail.com>
2026-02-25 18:04:47 -0300

    Solución de problema de scroll al cambiar de ruta.

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "../components/screens/Home/Home";
 import { EcolimpioPage } from "../components/screens/EcolimpioPage/EcolimpioPage";
+import ScrollToTop from "./ScrollToTop";
 
 export const AppRouter = () => {
     return (
         <>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Navigate to="/home" replace />} />
                 <Route path="/home" element={<Home />} />
